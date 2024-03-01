13d9b3898b6dbb4d55bce7a31221e3c3d1f07d62
newt <emailfornc@gmail.com>
2024-03-01 13:58:58 -0500

    Set default dropdown text and update onClick of dropdownmenu item

--- src/DeviceDropdown.js
+++ src/DeviceDropdown.js
@@ -3,14 +3,22 @@ import Dropdown from 'react-bootstrap/Dropdown';
 
 
 
-function Devices({ devices, onDeviceSelected, placeHolder }) {
+function Devices({ devices, onDeviceSelected, placeholder }) {
     const dropdownItems = devices.data.map((device) => {
-        return <Dropdown.Item key={device.model + device.id} href="#/action-1">{device.model + ' ID: ' + device.id}</Dropdown.Item>
+        return <Dropdown.Item
+            onClick={(e) => {
+                e.preventDefault();
+                console.log(device)
+                onDeviceSelected(device);
+            }}
+            key={device.model + device.id}
+            href="#/action-1">{device.model + ' ID: ' + device.id}
+        </Dropdown.Item>
     })
     return (
         <Dropdown>
             <Dropdown.Toggle variant="success" id="dropdown-basic">
-                {placeHolder}
+                {placeholder?.model || "Select A Device"}
             </Dropdown.Toggle>
 
             <Dropdown.Menu>
